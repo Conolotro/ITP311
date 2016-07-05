9acf17d4c878b40690e31820b998bb4493f083ad
Farhanaah <farhanaahishaak@gmail.com>
2016-07-05 14:29:29 +0800

    added bll and dll

---
+++ ITP311/App_Code/BLL/AccountBLL.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using ITP311.Entity_Class;
+using ITP311.DAL;
+using System.Collections;
+
+namespace ITP311.BLL
+{
+    public class AccountBLL
+    {
+            
+         public int CreateAccount(string NRIC, string fname, string lname, string PasswordHash, string PasswordSalt, string Email, string ContactNo, string Type)
+        {
+            AccountDAL acct = new AccountDAL();
+
+            try
+            {
+                return acct.CreateAccount(NRIC, fname, lname, PasswordHash, PasswordSalt, Email, ContactNo, Type);
+            }
+            catch
+            {
+                throw;
+            }
+             finally
+            {
+                acct = null;
+            }
+        }
+        
+
+     
+
+    }
+}
--- ITP311/App_Code/DAL/AccountDAL.cs
+++ ITP311/App_Code/DAL/AccountDAL.cs
@@ -1,32 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Linq;
 using System.Web;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using ITP311.BLL;
 
-namespace ITP311
+namespace ITP311.DAL
 {
     public class AccountDAL
     {
-        public string nric { get; set; }
-        public string passwordHash { get; set; }
-        public string salt { get; set; }
-        public string firstName { get; set; }
-        public string lastName { get; set; }
-        public int contactNo { get; set; }
 
-        String strConnectionString = ConfigurationManager.ConnectionStrings["itp311"].ToString();
+        string cn = ConfigurationManager.ConnectionStrings["medicalportalconnectionstring"].ToString();
 
-        public AccountDAL(string nric,string passwordHash,string salt,string firstName,string lastName,int contactNo)
+        public int CreateAccount(string NRIC, string fname, string lname, string PasswordHash, string PasswordSalt, string Email, string ContactNo, string Type)
         {
-            this.nric = nric;
-            this.passwordHash = passwordHash;
-            this.salt = salt;
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.contactNo = contactNo;
+            
+            SqlConnection conn = new SqlConnection(cn);
+            string sqlSelectString = "INSERT INTO Account(NRIC, fname, lname, PasswordHash, PasswordSalt, Email, ContactNo, Type) VALUES (@NRIC, @fname, @lname, @PasswordHash, @PasswordSalt, @Email, @ContactNo, @Type)";
+            SqlCommand cmd = new SqlCommand(sqlSelectString, conn);
+
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    cmd.Parameters.AddWithValue("@NRIC", NRIC);
+                    cmd.Parameters.AddWithValue("@fname", fname);
+                    cmd.Parameters.AddWithValue("@lname", lname);
+                    cmd.Parameters.AddWithValue("@PasswordHash", PasswordHash);
+                    cmd.Parameters.AddWithValue("@PasswordSalt", PasswordSalt);
+                    cmd.Parameters.AddWithValue("@Email", Email);
+                    cmd.Parameters.AddWithValue("@ContactNo", ContactNo);
+                    cmd.Parameters.AddWithValue("@Type", Type);
+                }
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return cmd.ExecuteNonQuery();
         }
 
+      
+
+        
+
+
+       
 
     }
-}
+
+    }
